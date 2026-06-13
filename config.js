fa427cf51eab69633e127e5adb5f6df646882d0f
edigitecomerce <edigitecomerce@gmail.com>
2026-06-13 09:49:53 -0300

    Primeiro

---
+++ config.js
@@ -0,0 +1,30 @@
+var cors = require('cors');
+const bodyParser = require('body-parser');
+var express = require('express');
+const logger = require('morgan');
+const app = express();
+multiparty = require('connect-multiparty');
+
+app.use(logger('dev'));
+app.use(express.static(__dirname + '/public'));
+
+const corsOptions = {
+     origin: '*',
+     method: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+     exposedHeaders: [
+          'Autorization',
+          'X-Requested-With',
+          'Content-Type',
+          'Cache-Control:no-cache'
+     ],
+     preflightContinue: false,
+
+};
+app.use(cors(corsOptions));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(multiparty());
+
+
+module.exports = { app};
---
+++ index.js
@@ -0,0 +1,58 @@
+const { app } = require('./config');
+
+
+app.get('/', async (req, res, next) => {
+
+    try {
+        const options = {
+            method: 'POST',
+            mode: 'no-cors',
+            origin: '*',
+            headers: {
+
+                accept: '*/*',
+                Authorization: 'Bearer b261e38c-4874-48ad-b20c-5d68baaaa3d6cfe4ea8d4ab1a67cf0c54c8e989f3e4232d0-66a8-4866-bda4-9bb8727221f0',
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                customer: {
+                    phone: {
+                        country: '+55',
+                        area: '27',
+                        number: '999999999'
+                    },
+                    name: 'João teste',
+                    email: 'joao@teste.com'
+                },
+                reference_id: '101',
+                expiration_date: '2026-06-13T23:45:00+03:00',
+                items: [{
+                    reference_id: '777',
+                    name: 'Nome do Produto',
+                    description: 'desc',
+                    quantity: 1,
+                    unit_amount: 520
+                }],
+                payment_methods: [{
+                    type: 'PIX'
+                }]
+            })
+        };
+        //Produtção: https://api.pagseguro.com/ 
+        fetch('https://sandbox.api.pagseguro.com/checkouts', options)
+            .then(res => res.json())
+            .then(json => { 
+                res.status(200).json(json)
+             })
+
+
+
+    } catch (err) {
+        next(err);
+    }
+});
+app.listen(8080, () => {
+
+    console.log('Connectado 8080')
+
+});

c68485ea3d88a382bd3bd805d28fe60574e5d0c3
edigitecomerce <edigitecomerce@gmail.com>
2026-08-10 11:27:38 -0300

    #impl - inativar pagamento

--- index.js
+++ index.js
@@ -1,43 +1,10 @@
 const { app } = require('./config');
 
-app.get('/api/payload', async (req, res, next) => {
-    try {
-        // Faz uma requisição para o seu servidor PHP rodando em outro endereço/porta
-        const response = fetch('https://edigitecomerce.free.nf/encarte-modelo-estab-fisico/admin/model/api/api.php?pg=get_pagamento');
-
-        // 2. Check if the HTTP status code is ok (200-299)
-        response.then((e) => { return e.text() })
-            .then((data) => {
-                console.log(JSON.stringify(data));
-            });
-
-        //res.status(200).json(response);
-    } catch (erro) {
-        res.status(500).json({ sucesso: false, erro: erro.message });
-    }
-
-});
-/*
-app.get('/get', async (req, res, next) => {
-
-    try {
-        const options = {
-            method: 'GET',
-            headers: { Authorization: 'Bearer b261e38c-4874-48ad-b20c-5d68baaaa3d6cfe4ea8d4ab1a67cf0c54c8e989f3e4232d0-66a8-4866-bda4-9bb8727221f0', accept: 'application/json' }
-        };
-        fetch('https://sandbox.api.pagseguro.com/checkouts/CHEC_884BC58A-C371-4715-9C92-66977AD0FC36', options)
-            .then(res => res.json())
-            .then(res => console.log(res))
-            .catch(err => console.error(err));
-    } catch (err) {
-        next(err);
-    }
-});*/
 
 app.post('/api/paguebank/', (req, res, next) => {
     try {
         var data = req.body;
-     
+
         const options = {
             method: 'POST',
             mode: 'no-cors',
@@ -60,7 +27,38 @@ app.post('/api/paguebank/', (req, res, next) => {
     } catch (err) {
         next(err)
     }
+});
+
+app.post('/api/paguebank/inativar/', (req, res, next) => {
+
+    const data = req.body;
+    var link_inativar = data.link;
+    var token = data.token;
+
+    try {
+        const options = {
+            method: 'POST',
+            headers: {
+
+                accept: '*/*',
+                Authorization: `Bearer ${data.token}`,
+                'Content-type': 'application/json'
+            }
+        };
+
+        fetch(link_inativar, options)
+        .then(res => res.json())
+        .then(element =>
+            res.json(element)
+        )
+        .catch(err => res.json(err));
+
+    } catch (err) {
+        next(err)
+    }
 })
+
+
 app.listen(8080, () => {
 
     console.log('Connectado 8080')
